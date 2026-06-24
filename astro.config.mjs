d11e8e247f49403ce42ecdbe4bcd590765ef1c90
agon1018 <hmyrs1031@gmail.com>
2026-06-24 19:56:30 +0900

    サイトマップを作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://the-hobby-garage.vercel.app',
+	integrations: [sitemap()],
+});
